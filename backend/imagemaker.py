30a77274aa434b2d20145472ffa579a2a5c51c11
nathan <nathan@nathan-thinkpad.(none)>
2013-01-19 15:25:16 -0500

    image stiching places properly

--- backend/imagemaker.py
+++ backend/imagemaker.py
@@ -2,30 +2,30 @@ import flashfoto
 import json, requests, urllib
 import base64, time
 
-def getBackground():
+#def getBackground():
     #get stuff from mike's flickr scraper
     #should be a url
-    return
+    #return
 
-def getForeground():
+#def getForeground():
     #get picture user uploads from filepicker.io
     #should be a url
-    return
+    #return
 
 def makePicture( fgroundURL , bgroundURL ):
     ff = flashfoto.FlashFoto('ncschaaf', 'DyRkKMSiYncpTaG2i7IuJy9FGA3bll5g')#not sure if i need to add a baseurl
     fgID = ff.add(params = {"location":base64.urlsafe_b64encode(fgroundURL), 'privacy':'public'})['ImageVersion']['image_id']
 
-    print
+    #print
     print "foreground id? " , fgID
-    print
-    ff.findfaces(fgID)
+    #print
+    #ff.findfaces(fgID)
 
     ff.mugshot(fgID)
     while ff.mugshot_status(fgID)['mugshot_status'] == 'pending' :
         time.sleep(10)
 
-    #print "\n STATUS " , ff.mugshot_status(fgID)
+    print "\n STATUS " , ff.mugshot_status(fgID)
 
     if ff.mugshot_status(fgID) == 'failed':
         assert False
@@ -40,12 +40,20 @@ def makePicture( fgroundURL , bgroundURL ):
     bgID = ff.add(params = {"location":base64.urlsafe_b64encode(bgroundURL), 'privacy':'public'})['ImageVersion']['image_id']
 
 
-    print
+    #print
     print "background added, with id " + bgID
-    print
+    #print
+
+    bgInfo = ff.info(bgID)
+    #print bgInfo
+    bgHeight = bgInfo['ImageVersion'][0]['height']
+    bgWidth = bgInfo['ImageVersion'][0]['width']
+    mergeX = int(bgWidth) / 3
+
+
     print 'merging...'
 
-    mergedata = [{"image_id":bgID}, {"image_id":fgID, "version":"MugshotMasked", "x":"200", "y":"200" , "scale":"75" , "angle":"0", "flip":0}]
+    mergedata = [{"image_id":bgID}, {"image_id":fgID, "version":"MugshotMasked", "x":mergeX, "y":bgHeight , "scale":"75" , "angle":"0", "flip":0}]
     mergedID = ff.merge(mergedata, params = {'privacy':'public'} )['ImageVersion']['image_id']
     return "http://flashfotoapi.com/api/get/" + mergedID +"?partner_username=ncschaaf&partner_apikey=DyRkKMSiYncpTaG2i7IuJy9FGA3bll5g"
     #return ff.get(mergedID)
