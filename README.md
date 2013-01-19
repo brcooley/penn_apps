6667f75aec419c86155192c18b349db6ee7aefc0
Peter Johnson <peter@pennapps.8862138807bd45ed9501a3a7497e4aae.pennapps.2972100199.useast.internal.cloudapp.net>
2013-01-19 18:22:53 +0000

    edited readme and worked on the python web interface

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-penn_apps 2013: Facation
+PennApps 2013: Facation
 ========================
 
 Plan a fake facation and do it on facebook blah blah blah Mike should write something here that makes this sound good when he wakes up from his nap.
@@ -7,31 +7,31 @@ Brainstorming
 =============
 
 generate vacation
-	kayak?
+* kayak?
 pretend to be away
-	pictures (with you ? tokbox; fileio?)
-	trip information
-	send texts? toivio
+* pictures (with you ? tokbox; fileio?)
+* trip information
+* send texts? toivio
 pretend to be home
-	things to do while youre hiding?
-		order in
-		movie reccommendations from netflix and rotten tomatores
+* things to do while youre hiding?
+* order in
+* movie reccommendations from netflix and rotten tomatores
 
 
 
 webpage with what is going to happen
-	login with facebook / permissions
-	approve vacation
+* login with facebook / permissions
+* approve vacation
 mongo db to store logins and vacations (for time delay)
-	allow cancel vacations
+* allow cancel vacations
 
 
-
----------------API--------------------
-masherly (hotwire)
-facebook
-mongoDB
-azure? (hosting)
-filepicker.io? tumblr? (get social pictues)
-twilio? (send fake texts; notify users about updates to their vacations)
-nytimes (what book are you reading?)
+API
+===
+* masherly (hotwire)
+* facebook
+* mongoDB
+* azure? (hosting)
+* filepicker.io? tumblr? (get social pictues)
+* twilio? (send fake texts; notify users about updates to their vacations)
+* nytimes (what book are you reading?)
--- webhandler.py
+++ webhandler.py
@@ -1,3 +1,13 @@
+'''
+take access_token ip
+    return random location, pictures, flight info, hotel
+take photo,
+    return filepicker'd pictures
+'''
+
+import json
+import os
+import random
 import urllib
 
 import pymongo
@@ -5,32 +15,52 @@ import requests
 import web
 
 urls = (
-    '/extend_token', 'extend_token',
+        '/vacationinfo', 'vacation_info',
     )
 
-class extend_token:
-    def GET(self, access_token):
-        print access_token
-        return
-        url = ('https://graph.facebook.com/oauth/access_token?' + \
-            'grant_type=fb_exchange_token&' \
-            'client_id=%s&' \
-            'client_secret=%s&' \
-            'fb_exchange_token=%s') % (app_id, app_secret, access_token)
-        dct = dict(x.split('=') for x in requests.get(url).text.split('&'))
-        new_access_token = dct['access_token']
-        # Update all tables to reflect the new access token.
-        conn = pymongo.MongoClient('localhost', 27017) 
-        db = conn.facation
-        db.collections.update(
-            { 'access_token': new_access_token },
-            { 'access_token': access_token })
-        conn.close()
-        return new_access_token
+class vacation_info:
+    def GET(self):
+        print web.input()
+        return self.POST()
+    def POST(self):
+        print web.data()
+        location = choose_location() 
+        ip_address = web.ctx.ip
+        return json.dumps({
+            'location': location,
+            'photos': None,
+            'flights': None,
+            'hotels': None,
+            'books': None,
+            })
 
+def choose_location():
+    return random.choice((
+        'Honolulu, Hawaii',
+        'New York City, New York',
+        'Prague, Czech Republic',
+        'Bankok, Thailand',
+        'Williamsburg, Virginia',
+        ))
+
+def extend_token(access_token):
+    url = ('https://graph.facebook.com/oauth/access_token?' + \
+        'grant_type=fb_exchange_token&' \
+        'client_id=%s&' \
+        'client_secret=%s&' \
+        'fb_exchange_token=%s') % (app_id, app_secret, access_token)
+    dct = dict(x.split('=') for x in requests.get(url).text.split('&'))
+    new_access_token = dct['access_token']
+    # Update all tables to reflect the new access token.
+    conn = pymongo.MongoClient('localhost', 27017) 
+    db = conn.facation
+    db.collections.update(
+        { 'access_token': new_access_token },
+        { 'access_token': access_token })
+    conn.close()
+    return new_access_token
+ 
 
 if __name__ == '__main__':
     app = web.application(urls, globals())
     app.run()
-
-
