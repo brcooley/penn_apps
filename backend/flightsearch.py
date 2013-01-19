68358c4ec38523e6ddcef1ec9d6c9b7e6f795694
brc <brett@cooleyweb.org>
2013-01-19 15:23:39 -0500

    Adding POST URL

--- backend/flightsearch.py
+++ backend/flightsearch.py
@@ -4,5 +4,5 @@ def main():
 	csrf = requests.get('http://www.hipmunk.com')
 	search_strings = { 'i': 'Philidelphia+PA.Denver+CO,Jan26.Feb2' }
 	headers = { 'X-Csrf-Token': csrf.headers['X-Csrf-Token'] }
-	raw_data = requests.post(data=search_strings, headers=headers)
+	raw_data = requests.post('http://www.hipmunk.com/api/results', data=search_strings, headers=headers)
 	print(raw_data.json()['routings'])
