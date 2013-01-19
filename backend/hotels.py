7ccd40bbb9f4c1e65dd326372c36d5a97bd423f4
nathan <nathan@nathan-thinkpad.(none)>
2013-01-19 17:54:25 -0500

    fixed stupid mystery error

--- backend/hotels.py
+++ backend/hotels.py
@@ -1,16 +1,16 @@
-# coding: utf-8
 import json, requests, urllib
 
 # dest is a string which contains a city name. It can also have country, state, etc.
 # all dates MM/DD/YYYY
-def pickHotel( dest, arrival, departure ):
-    url = "http://api.ean.com/ean‑services/rs/hotel/v3/list"
-    payload = {'minorRev':'20', 'apiKey':'j6xun3rbjn66vhzhvnm99twg', 'cid':'55505', 'numberOfResults':'1', 'minStarRating':'4', 'destinationString':dest, 'room1':'2', 'arrivalDate':arrival, 'departureDate':departure}
+def pickHotel( dest, arrival, departure, whichHotel=0):
+    url = 'http://api.ean.com/ean-services/rs/hotel/v3/list'
+    payload = {'apiKey':'j6xun3rbjn66vhzhvnm99twg', 'cid':'55505', 'destinationString':dest , 'minStarRating':'4', }
     #print url
     hotelInfo = requests.get(url, params=payload)
     print hotelInfo.url
-    print hotelInfo
-    #return hotelID
+    hotelSummary = hotelInfo.json()['HotelListResponse']['HotelList']['HotelSummary'][whichHotel]
+    
+    return hotelID
 
 
 pickHotel("Boston", '01/27/2013', '01/29/2013')
