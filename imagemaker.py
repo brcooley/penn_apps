057b550d4f0443eff1ebfeb6bee05bf1b0dc812f
nathan <nathan@nathan-thinkpad.(none)>
2013-01-19 13:22:12 -0500

    still broken image stiching; lots of print statements

--- imagemaker.py
+++ imagemaker.py
@@ -1,31 +1,47 @@
-import flashfoto.py
+import flashfoto
 import json, requests, urllib
+import base64, time
 
 def getBackground():
     #get stuff from mike's flickr scraper
     #should be a url
+    return
 
 def getForeground():
     #get picture user uploads from filepicker.io
     #should be a url
+    return
 
 def makePicture( fgroundURL , bgroundURL ):
-    ff = FlashFoto('ncschaaf', 'DyRkKMSiYncpTaG2i7IuJy9FGA3bll5g')#not sure if i need to add a baseurl
-    fgID = ff.add(params = getForeground()) #might need to parse out image id; not sure exactly what is returned by this method
-    print "foreground id? "+ fgID
+    ff = flashfoto.FlashFoto('ncschaaf', 'DyRkKMSiYncpTaG2i7IuJy9FGA3bll5g')#not sure if i need to add a baseurl
+    fgID = ff.add(params = {"location":base64.urlsafe_b64encode(fgroundURL), 'privacy':'public'})['ImageVersion']['image_id']
+
+    print
+    print "foreground id? " , fgID
+    print
+
+
     ff.mugshot(fgID)
     while ff.mugshot_status(fgID) == 'pending' :
-        pass
-    print ff.mugshot_status(fgID)
+        time.sleep(2)
+    print "\n STATUS " , ff.mugshot_status(fgID)
 
     if ff.mugshot_status(fgID) == 'failed':
         assert False
 
-    bgID = ff.add(params = getBackground())#need parsing??
+    print "adding background..."
+
+    bgID = ff.add(params = {"location":base64.urlsafe_b64encode(bgroundURL), 'privacy':'public'})['ImageVersion']['image_id']
+
+
+    print
+    print "background added, with id " + bgID
+    print
+    print 'merging...'
 
     mergedata = [{"image_id":bgID}, {"image_id":fgID, "version":"MugshotMasked", "x":"200", "y":"100" , "scale":"75" , "angle":"0", "flip":0}]
     return ff.merge(mergedata)# might want to bring this out to a url; make it easier to proccess elsewhere
     #return ff.get(mergedID)
     
     
-makePicture(http://flashfotoapi.com/img/tut_mugshot1.png , )
+print makePicture("http://us.123rf.com/400wm/400/400/gelpi/gelpi0705/gelpi070500114/926294-attractive-young-person-businessman-a-over-white-background.jpg" , "http://farm9.staticflickr.com/8334/8391368003_b4880e7de5_h.jpg")
