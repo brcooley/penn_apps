d07f4e2568b5881db16931bfe0137d60b0af795d
nathan <nathan@nathan-thinkpad.(none)>
2013-01-19 02:18:35 -0500

    i want to share with peter

--- airport.py
+++ airport.py
@@ -1,16 +1,24 @@
 import json, requests, urllib
+import csv
+import math
 
 def getGeoInfo( ip ):
     url = "http://freegeoip.net/json/" + str(ip)
     result = requests.get(url)
     return result.json()
 
-
+def distance( x1 , y1 , x2 , y2 ):
+   return  math.sqrt(math.abs((x1-x2)**2 + (y1-y2)**2))
 
 def main():
     geo = getGeoInfo('158.130.102.99')
-    print "lat:  "+ geo['latitude']
-    print 'long: ' + geo['longitude']
+    lat = geo['latitude']
+    lon = geo['longitude']
+    print "lat:  "+ lat
+    print 'long: ' + lon
+    airports = csv.DictReader('airports.dat')
+    minDistance = min([distance(lat, lon, entry['LATITUDE'], entry['LONGITITUDE']) for entry in airports])
+    print "min distance: " +  minDistance
 
 
 main()
