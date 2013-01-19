24d0c98ee064632b622c8cf0e0533679e4aa54bd
nathan <nathan@nathan-thinkpad.(none)>
2013-01-19 09:46:24 -0500

    preparing image composition

---
+++ imagemaker.py
@@ -0,0 +1,13 @@
+import flashfoto.py
+import json, requests, urllib
+
+def getBackground():
+    #get stuff from mike's flickr scraper
+    #should be a url
+
+def getForeground():
+    #get picture user uploads from filepicker.io
+    #should be a url
+
+def makePicture( fgroundURL , bgroundURL ):
+    
