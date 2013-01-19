1e8e42a0314c72cae2fb6151547de4ab6b9c61af
nathan <nathan@nathan-thinkpad.(none)>
2013-01-19 18:04:55 -0500

    only 4 stars and higher

--- backend/hotels.py
+++ backend/hotels.py
@@ -4,13 +4,13 @@ import json, requests, urllib
 # all dates MM/DD/YYYY
 def pickHotel( dest, arrival, departure, whichHotel=0):
     url = 'http://api.ean.com/ean-services/rs/hotel/v3/list'
-    payload = {'apiKey':'j6xun3rbjn66vhzhvnm99twg', 'cid':'55505', 'destinationString':dest , 'minStarRating':'4', }
+    payload = {'apiKey':'j6xun3rbjn66vhzhvnm99twg', 'cid':'55505', 'destinationString':dest , 'minStarRating':'4.0', }
     #print url
     hotelInfo = requests.get(url, params=payload)
     print hotelInfo.url
     hotelSummary = hotelInfo.json()['HotelListResponse']['HotelList']['HotelSummary'][whichHotel]
     
-    return hotelID
+    #return hotelID
 
 
 pickHotel("Boston", '01/27/2013', '01/29/2013')
