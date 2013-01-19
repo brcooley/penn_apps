c4db30376eac7643f8071f92d76089ee5b554646
mmpozulp <mmpozulp@email.wm.edu>
2013-01-19 06:08:03 -0500

    added scripts to parse nytimes bestesllers

---
+++ nybest.py
@@ -0,0 +1,26 @@
+#!/Library/Frameworks/Python.framework/Versions/2.7/bin/python
+# nybest.py
+# @author Mike
+# queries nytimes api and asks for a best seller; currently uses
+# the first listing from the query
+
+from bestsellers import BestSellers
+import pprint
+import simplejson
+
+api_key = 'bc909254c6c2e7d4b9a73267f33d4cd8:10:67204552'
+genre = 'hardcover-fiction'
+genre = 'combined-print-and-e-book-fiction'
+WHICH_ENTRY = 0
+
+b = BestSellers(api_key)
+book_list = b.get_list(genre)
+
+
+# the only variable is WHICH_ENTRY, everything else should be
+# hardcoded ------>  book_list[WHICH ENTRY][u 'book_details'][0][u'title']
+
+#print book_list[WHICH_ENTRY][u'book_details'][0][u'title']
+for book in book_list:
+   print book[u'book_details'][0][u'title']
+
---
+++ scrape.tcsh
@@ -0,0 +1,6 @@
+#!/bin/tcsh
+# scrape.tcsh
+# @author Mike
+# write scrape.py to scrape.out
+
+python scrape.py >> scrape.out &
